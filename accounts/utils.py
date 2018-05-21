9f8405305c8be0e1195be2913e33507d638ec299
pedro rivera <ppsirg@gmail.com>
2018-05-21 02:06:34 -0500

    added reports to homebank compatibility

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,9 +1,13 @@
 from accounts.models import account, transaction
+from accounts.models import transactionCategory
+from persons.models import owner
+from pprint import pprint
 
 
 def calculate_balance(account_object):
     response = calculate_balance_report(account_object)
     return response['balance']
+
 
 def calculate_balance_report(account_object):
     transaction_list = transaction.objects.filter(account=account_object)
@@ -22,3 +26,38 @@ def calculate_balance_report(account_object):
         'expenses': expenses,
         'incomes': incomes
         }
+
+
+def calculate_homebank_report(user_obj):
+    """
+    calculate resume for homebank sync
+    """
+    report = {}
+    q_list = transaction.objects.filter(account=account.objects.get(owner=user_obj))
+    for item in q_list:
+        if item.category.name in report:
+            report[item.category.name]['value'] += item.value
+            report[item.category.name]['detail'].append(item.description)
+        else:
+            report[item.category.name] = {'value': item.value, 'detail': [item.description,]}
+    for item in report:
+        report[item]['detail'] = ';'.join(report[item]['detail'])
+    return report
+
+
+def reset_user_account_transactions(user_obj, initial_value):
+    """
+    delete transactions of an user and sets an initial value for account
+    """
+    cuenta = account.objects.get(owner=user_obj)
+    tx_category = transactionCategory.objects.get(name='transfer')
+    q_list = transaction.objects.filter(account=cuenta)
+    for item in q_list:
+        item.delete()
+    tx = transaction.objects.create(
+        value=initial_value,
+        category=tx_category,
+        description='saldo inicial',
+        account=cuenta,
+    )
+    tx.save()
